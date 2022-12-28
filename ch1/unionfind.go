8aad6ae8be51eec8c8709e6bb32f220950320514
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2022-12-28 00:27:51 -0500

    unionfind more

--- ch1/unionfind.go
+++ ch1/unionfind.go
@@ -7,6 +7,8 @@ type EqClass struct {
 	Father	*EqClass
 }
 
+type EqRelation struct { A, B *EqClass }
+
 func Union(p, q *EqClass) *EqClass {
 	if p != nil && p.Data != q.Data {
 		p.Father = q
@@ -23,10 +25,32 @@ func Find(p *EqClass) *EqClass {
 
 	// short-circuit the path by linking ec to r
 	var father *EqClass
-	for ec := p; ec != nil; ec = father {
+	for ec := p; ec.Father != nil; ec = father {
 		father = ec.Father
 		ec.Father = r
 	}
 
 	return r
-} 
+}
+
+func MakeSet(e int) *EqClass {
+	return &EqClass{e, nil}
+}
+
+func UnionFind(eqrels []*EqRelation) []*EqClass {
+	m := make(map[*EqClass]bool)
+	for _, rel := range eqrels {
+		a := Find(rel.A)
+		b := Find(rel.B)
+		if a != b {
+			m[Union(a, b)] = true
+		} else {
+			m[a], m[b] = true, true
+		}
+	}
+	var s []*EqClass
+	for k, _ := range m {
+		s = append(s, k)
+	}
+	return s
+}
