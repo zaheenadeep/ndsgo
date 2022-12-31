65ddca4d77fc8a21f128755d6edac7b59166a721
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2022-12-31 02:36:48 -0500

    unionfind clutter cleared

--- ch1/unionfind.go
+++ ch1/unionfind.go
@@ -37,68 +37,38 @@ func MakeSet(e int) *EqClass {
 	return &EqClass{e, nil}
 }
 
-func UnionFind(rels []*EqRelation) []*EqClass {
-	s := NewSet()
+func UnionFind(rels []*EqRelation) {
 	for _, rel := range rels {
-		a := Find(rel.A)
-		b := Find(rel.B)
-		s.Remove(a)
-		s.Remove(b)
-		if a != b {	
-			s.Add(Union(a, b))
-		} else {
-			s.Add(a)
+		a, b := Find(rel.A), Find(rel.B)
+		if a != b {
+			Union(a, b)
 		}
 	}
-
-	var keys []*EqClass
-	for k := range s.list {
-		keys = append(keys, k)
-	}
-	return keys
 }
 
 func main() {
 	var (
-		Two		= MakeSet(2)
-		Three	= MakeSet(3)
-		Seven	= MakeSet(7)
+		One			= MakeSet(1)
+		Two			= MakeSet(2)
+		Three		= MakeSet(3)
+		Four		= MakeSet(4)
+		Five		= MakeSet(5)
+		Seven		= MakeSet(7)
+		Twelve		= MakeSet(12)
+		ThirtyTwo	= MakeSet(32)
+		ThirtyFour	= MakeSet(34)
+		SixtyFour	= MakeSet(64)
+		SeventyFive	= MakeSet(75)
+		NinetyNine	= MakeSet(99)
 	)
 	rels := []*EqRelation{
-		{MakeSet(1), Two}, {Three, Two}, {MakeSet(4), Three}, // equivalent
-		{MakeSet(75), MakeSet(12)}, {MakeSet(32), MakeSet(64)}, {MakeSet(34), MakeSet(99)}, // distinct
-		{Seven, Seven}, {Seven, MakeSet(5)}, // equivalent
+		{One, Two}, {Three, Two}, {Four, Three}, // equivalent
+		{SeventyFive, Twelve}, {ThirtyTwo, SixtyFour}, {ThirtyFour, NinetyNine}, // distinct
+		{Seven, Seven}, {Seven, Five}, // equivalent
 	}
+	UnionFind(rels)
 
-	uf := UnionFind(rels)
-	for _, ec := range uf {
-		fmt.Println(ec.Data)
-	}
-}
-
-// Ignore everything after this.
-// It is an abstraction defined for set operations using built-in go maps.
-// from https://gist.github.com/bgadrian/cb8b9344d9c66571ef331a14eb7a2e80
-
-type Set struct {
-	list map[*EqClass]struct{} //empty structs occupy 0 memory
-}
-
-func (s *Set) Has(v *EqClass) bool {
-	_, ok := s.list[v]
-	return ok
-}
-
-func (s *Set) Add(v *EqClass) {
-	s.list[v] = struct{}{}
-}
-
-func (s *Set) Remove(v *EqClass) {
-	delete(s.list, v)
-}
-
-func NewSet() *Set {
-	s := &Set{}
-	s.list = make(map[*EqClass]struct{})
-	return s
+	// Tests
+	fmt.Println(Find(Two) == Find(One)) // true; Two and One equivalent
+	fmt.Println(Find(Two) == Find(Twelve))  // false; Two and Twelve not equivalent
 }
