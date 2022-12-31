a5ca9f78b1b2b882e333456a8f3a6ef889c9a17a
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2022-12-31 02:41:47 -0500

    more examples

--- ch1/unionfind.go
+++ ch1/unionfind.go
@@ -69,7 +69,8 @@ func main() {
 	UnionFind(rels)
 
 	// Tests
-	fmt.Println(Find(Two) == Find(One)) // true; Two and One equivalent
+	fmt.Println(Find(Two) == Find(Three)) // true; Two and One equivalent
+	fmt.Println(Find(SeventyFive) == Find(Twelve)) // true; SeventyFive and Twelve equivalent
 	fmt.Println(Find(Two) == Find(Twelve))  // false; Two and Twelve not equivalent
 	fmt.Println(Find(Seven) == Find(MakeSet(1000))) // false: Seven and new class not equivalent
 }
