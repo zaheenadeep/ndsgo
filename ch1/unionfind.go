7f9e0cfda2929b01d6fd18c468f7ec95b256d82b
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2022-12-31 01:57:35 -0500

    unionfind works with wonky set notation using map

--- ch1/unionfind.go
+++ ch1/unionfind.go
@@ -37,20 +37,41 @@ func MakeSet(e int) *EqClass {
 	return &EqClass{e, nil}
 }
 
-func UnionFind(eqrels []*EqRelation) []*EqClass {
-	m := make(map[*EqClass]bool)
-	for _, rel := range eqrels {
+func UnionFind(rels []*EqRelation) []*EqClass {
+	m := make(map[*EqClass]bool) // use built-in map as a set
+	for _, rel := range rels {
 		a := Find(rel.A)
 		b := Find(rel.B)
-		if a != b {
-			m[Union(a, b)] = true
+		delete(m, a) // set remove
+		delete(m, b) // set remove
+		if a != b {	
+			m[Union(a, b)] = true // set add
 		} else {
-			m[a], m[b] = true, true
+			m[a] = true // set add
 		}
 	}
-	var s []*EqClass
-	for k, _ := range m {
-		s = append(s, k)
+
+	var keys []*EqClass
+	for k := range m {
+		keys = append(keys, k)
 	}
-	return s
+	return keys
+}
+
+func main() {
+	var (
+		Two		= MakeSet(2)
+		Three	= MakeSet(3)
+		Seven	= MakeSet(7)
+	)
+	rels := []*EqRelation{
+		{MakeSet(1), Two}, {Three, Two}, {MakeSet(4), Three}, // equivalent
+		{MakeSet(75), MakeSet(12)}, {MakeSet(32), MakeSet(64)}, {MakeSet(34), MakeSet(99)}, // distinct
+		{Seven, Seven}, {Seven, MakeSet(5)}, // equivalent
+	}
+
+	uf := UnionFind(rels)
+	for _, ec := range uf {
+		fmt.Println(ec.Data)
+	}
 }
