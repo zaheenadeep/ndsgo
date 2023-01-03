12490e12cd86b2fb10b763b765771aad630c5baa
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2023-01-03 00:17:22 -0500

    Comments for rangetree

--- ch7/rangetree.go
+++ ch7/rangetree.go
@@ -1,4 +1,6 @@
-package rangetree
+package main
+
+import "fmt"
 
 type RType int
 
@@ -7,14 +9,14 @@ const (
 	NonLeaf
 )
 
-type RNode {
+type RNode struct {
 	Type	RType
 	Mid		int
 	Left	*RNode
 	Right	*RNode
 }
 
-func (t *Rnode) RangeSearch(b, e int) []int {
+func (t *RNode) RangeSearch(b, e int) []int {
 	// tree traversal
 	for t != nil && t.Type == NonLeaf {
 		if b <= t.Mid {
@@ -23,7 +25,7 @@ func (t *Rnode) RangeSearch(b, e int) []int {
 			t = t.Right
 		}
 	}
-	if t == nil {
+	if t == nil { // t beyond last leaf node
 		return nil
 	}
 
@@ -37,4 +39,20 @@ func (t *Rnode) RangeSearch(b, e int) []int {
 		t = t.Right
 	}
 	return s
+}
+
+func main() {
+	t := &RNode{Leaf, 32, nil, nil}
+	fmt.Println(t.RangeSearch(32, 45))
+	t = &RNode{Leaf, 20, nil, nil}
+	fmt.Println(t.RangeSearch(32, 45))
+
+	c1 := &RNode{Leaf, 30, nil, nil}
+	c2 := &RNode{Leaf, 50, c1, nil}
+	c1.Right = c2
+	t = &RNode{NonLeaf, 40, c1, c2}
+	fmt.Println(t.RangeSearch(20, 45))
+	fmt.Println(t.RangeSearch(20, 95))
+	fmt.Println(t.RangeSearch(40, 95))
+	fmt.Println(t.RangeSearch(95, 100))
 }
